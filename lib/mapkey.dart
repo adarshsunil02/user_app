6f78035b9e1207ef533b20584b72bf8a9c7515d7
adarsh sunil <adarsh02sunil@gmail.com>
2024-06-20 14:40:52 +0530

    second commit

--- lib/mapkey.dart
+++ lib/mapkey.dart
@@ -1,3 +1,1 @@
-String mapKey="AIzaSyC2Ynm0wRGz9h5P3tPnjwLcgmiLzuzqqcg";
-const String GOOGLE_MAPS_API_KEY = "AIzaSyC2Ynm0wRGz9h5P3tPnjwLcgmiLzuzqqcg";
-const String googleMapsApiKey = "AIzaSyC2Ynm0wRGz9h5P3tPnjwLcgmiLzuzqqcg";
+String mapKey="API_KEY_HERE";
